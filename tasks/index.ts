5dd117ecd1f95f775f6fffd51f310e73a61f8bb6
khang <khangtd@solashi.com>
2025-05-22 10:32:26 +0700

    update

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,57 +1,58 @@
-export * from "./query";
-export * from "./req";
-export * from "./res";
+export * from './query';
+export * from './req';
+export * from './res';
 
-import { initContract } from "@ts-rest/core";
-import type { IGetManyRes, IRes } from "../common";
-import { taskQuerySchema } from "./query";
-import { createTaskSchema, updateTaskSchema } from "./req";
-import type { ITask } from "./res";
+import { initContract } from '@ts-rest/core';
+import type { IGetManyRes, IRes } from '../common';
+import { taskQuerySchema } from './query';
+import { createTaskSchema, updateTaskSchema } from './req';
+import type { ITask } from './res';
 
 const c = initContract();
 
 export const taskContract = c.router(
   {
     createTask: {
-      method: "POST",
-      path: "/",
+      method: 'POST',
+      path: '/',
+      contentType: 'multipart/form-data',
       body: createTaskSchema,
       responses: {
         200: c.type<ITask>(),
       },
     },
     updateTask: {
-      method: "PATCH",
-      path: "/:id",
+      method: 'PATCH',
+      path: '/:id',
       body: updateTaskSchema,
       responses: {
         200: c.type<ITask>(),
       },
     },
     getTasks: {
-      method: "GET",
-      path: "/",
+      method: 'GET',
+      path: '/',
       query: taskQuerySchema,
       responses: {
         200: c.type<IGetManyRes<ITask>>(),
       },
     },
     getTask: {
-      method: "GET",
-      path: "/:id",
+      method: 'GET',
+      path: '/:id',
       responses: {
         200: c.type<ITask>(),
       },
     },
     deleteTask: {
-      method: "DELETE",
-      path: "/:id",
+      method: 'DELETE',
+      path: '/:id',
       responses: {
         200: c.type<IRes>(),
       },
     },
   },
   {
-    pathPrefix: "/tasks",
-  }
+    pathPrefix: '/tasks',
+  },
 );
--- tasks/req.ts
+++ tasks/req.ts
@@ -1,10 +1,16 @@
-import { z } from "zod";
+import { z } from 'zod';
 
-export const createTaskSchema = z.object({
+export const createTaskSchemaForm = z.object({
   name: z.string(),
   description: z.string().optional(),
   dueDate: z.coerce.date().optional(),
   attachments: z.array(z.instanceof(File)).optional(),
+});
+export const createTaskSchema = z.object({
+  name: z.string(),
+  description: z.string().optional(),
+  dueDate: z.coerce.date().optional(),
+  attachments: z.array(z.string()).optional(),
 });
 export type CreateTaskSchema = z.infer<typeof createTaskSchema>;
 
