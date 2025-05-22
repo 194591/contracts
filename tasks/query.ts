b2a1cd8738eb0fe693bfab5b679a4bbd1303b941
khang <khangtd@solashi.com>
2025-05-22 14:31:27 +0700

    update

--- tasks/query.ts
+++ tasks/query.ts
@@ -1,5 +1,8 @@
 import { z } from 'zod';
 import { baseQuerySchema } from '../common';
 
-export const taskQuerySchema = baseQuerySchema.extend({});
+export const taskQuerySchema = baseQuerySchema.extend({
+  orderBy: z.enum(['id', 'name']).optional(),
+  order: z.enum(['asc', 'desc']).optional(),
+});
 export type ITaskQuery = z.infer<typeof taskQuerySchema>;
