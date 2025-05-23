ddf07bf7567c3795ebed844c91408bd094d2be83
khang <khangtd@solashi.com>
2025-05-23 11:55:01 +0700

    format

--- tasks/query.ts
+++ tasks/query.ts
@@ -1,18 +1,18 @@
-import { z } from "zod";
-import { baseQuerySchema } from "../common";
+import { z } from 'zod';
+import { baseQuerySchema } from '../common';
 
 const _taskOrderBy = [
-  "id",
-  "name",
-  "dueDate",
-  "createdAt",
-  "updatedAt",
+  'id',
+  'name',
+  'dueDate',
+  'createdAt',
+  'updatedAt',
 ] as const;
 
 export type ITaskOrderBy = (typeof _taskOrderBy)[number];
 
 export const taskQuerySchema = baseQuerySchema.extend({
   orderBy: z.enum(_taskOrderBy).optional(),
-  order: z.enum(["asc", "desc"]).optional(),
+  order: z.enum(['asc', 'desc']).optional(),
 });
 export type ITaskQuery = z.infer<typeof taskQuerySchema>;
