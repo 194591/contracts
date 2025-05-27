501ae69b5de07a32c5de0a378ecd8faef4e0e458
khang <khang194591@gmail.com>
2025-05-28 00:19:36 +0700

    update

--- users/query.ts
+++ users/query.ts
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { z } from 'zod';
 import { baseQuerySchema } from '../common';
 
 export const querySimpleUserSchema = baseQuerySchema.extend({
-  notInIds: z.string().array().optional(),
+  'notInIds[]': z.preprocess(
+    (val) => (Array.isArray(val) ? val : [val]),
+    z.string().array().optional(),
+  ),
 });
 export type TQuerySimpleUser = z.infer<typeof querySimpleUserSchema>;
